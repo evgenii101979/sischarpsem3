bc9341a5922aa277b27d531cd74144f4d7e25407
evgenii <evgenii.lyabov@mail.ru>
2022-09-22 20:56:04 +0300

    add sem3xychetvert

---
+++ sem3xychetvert/Program.cs
@@ -0,0 +1,21 @@
+﻿// прога ввод x и y x!=0 y!=0 вывод четверти где точка
+Console.WriteLine("Введите x ");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите y ");
+int y = int.Parse(Console.ReadLine());
+if(x > 0 && y > 0)
+{
+    Console.WriteLine("1");
+    }
+    else if(x < 0 && y < 0);
+    {
+    Console.WriteLine("3");
+    }
+    else if(x < 0 && y > 0);
+    {
+    Console.WriteLine("4");
+    }
+    else(x > 0 && y < 0);
+    {
+    Console.WriteLine("2");
+}
